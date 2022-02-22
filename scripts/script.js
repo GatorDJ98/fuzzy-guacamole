9a02b4f4d4dacd43bc0797e484c92fdd508f38e4
GatorDJ98 <97544775+GatorDJ98@users.noreply.github.com>
2022-02-22 12:14:52 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const info = ["dodle@una.edu", "CIS-376", "University of North Alabama"];
 
 
 
-for (j = 0; j < cumbieArray.length; j++) {
+for (j = 0; j < info.length; j++) {
     //do something
-    $("#arrayOutput").append("a href=/" + cumbieArray[j] + "/"" <br/>");
+    $("#arrayOutput").append(info[j] + "<br/>");
   }
